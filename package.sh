e298936524fc3259b8624e54c0e2a6353b837368
Nicholas Anderson <nander13@gmu.edu>
2014-04-28 17:11:36 -0400

    Cleaning more formatting

--- package.sh
+++ package.sh
@@ -15,7 +15,6 @@ mv ${SCRIPTPATH}/src/app.nw ${SCRIPTPATH}/app.nw
 
 if [ -d ${SCRIPTPATH}/node-webkit/linux-x64 ]; then
 	echo "Building Linux64 version..."
-	# Build the linux64 version
 	mkdir -p ${SCRIPTPATH}/packaged/linux-x64/
 	cat ${SCRIPTPATH}/node-webkit/linux-x64/nw ${SCRIPTPATH}/app.nw > ${SCRIPTPATH}/packaged/linux-x64/ohsnap_gui
 	cp ${SCRIPTPATH}/node-webkit/linux-x64/nw.pak ${SCRIPTPATH}/packaged/linux-x64/nw.pak 2>/dev/null
@@ -23,7 +22,6 @@ if [ -d ${SCRIPTPATH}/node-webkit/linux-x64 ]; then
 fi
 if [ -d ${SCRIPTPATH}/node-webkit/linux-ia32 ]; then
 	echo "Building Linux32 version..."
-	# Build the linux32 version
 	mkdir -p ${SCRIPTPATH}/packaged/linux-ia32/
 	cat ${SCRIPTPATH}/node-webkit/linux-ia32/nw ${SCRIPTPATH}/app.nw > ${SCRIPTPATH}/packaged/linux-ia32/ohsnap_gui
 	cp ${SCRIPTPATH}/node-webkit/linux-ia32/nw.pak ${SCRIPTPATH}/packaged/linux-ia32/nw.pak 2>/dev/null
@@ -34,7 +32,6 @@ if [ -d $SCRIPTPATH/node-webkit/osx-ia32 ]; then
 fi
 if [ -d ${SCRIPTPATH}/node-webkit/win-ia32 ]; then
 	echo "Building Win32 version..."
-	# Build the win32 version
 	mkdir -p ${SCRIPTPATH}/packaged/win-ia32/
 	cat ${SCRIPTPATH}/node-webkit/win-ia32/nw.exe ${SCRIPTPATH}/app.nw > ${SCRIPTPATH}/packaged/win-ia32/ohsnap_gui.exe
 	cp -q ${SCRIPTPATH}/node-webkit/win-ia32/{nw.pak,icudt.dll,libEGL.dll,libGLESv2.dll,d3dx9_43.dll,D3DCompiler_43.dll} ${SCRIPTPATH}/packaged/win-ia32/ 2>/dev/null
