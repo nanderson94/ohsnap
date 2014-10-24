e24ca8b596cb7f64e56252c98934f2860eeec763
Nicholas Anderson <nander13@gmu.edu>
2014-10-24 13:24:21 -0400

    Pass all arguments

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ if [ "$(uname)" == "Darwin" ]; then
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 	if [ "$(uname -m)" == "x86_64" ]; then
 		echo "Running linux-x64";
-		${BASH_SOURCE%/*}/node-webkit/linux-x64/nw ${BASH_SOURCE%/*}/src/
+		${BASH_SOURCE%/*}/node-webkit/linux-x64/nw ${BASH_SOURCE%/*}/src/ "$@"
 	else
 		echo "Running linux-ia32";
-		${BASH_SOURCE%/*}/node-webkit/linux-ia32/nw ${BASH_SOURCE%/*}/src/
+		${BASH_SOURCE%/*}/node-webkit/linux-ia32/nw ${BASH_SOURCE%/*}/src/ "$@"
 	fi
 	echo -e "\n\nNOTICE: If you are getting a libudev error, check:";
 	echo "https://github.com/rogerwang/node-webkit/wiki/The-solution-of-lacking-libudev.so.0";
